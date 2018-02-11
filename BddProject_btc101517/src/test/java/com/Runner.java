0adc6cb06fa503ffe0cc791dde8c27fbc27b362f
Bittech Consulting <bittechconsulting@Bittechs-iMac-2.home>
2018-02-11 17:24:04 -0500

    modify project

--- BddProject_btc101517/src/test/java/com/Runner.java
+++ BddProject_btc101517/src/test/java/com/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features= {"/Users/bittechconsulting/eclipse-workspace/BddProject_btc101517/param.feature"}	
+@CucumberOptions(features= {"/Users/bittechconsulting/eclipse-workspace/BddProject_btc101517/param.feature"},dryRun=true
 )
 
 public class Runner {
